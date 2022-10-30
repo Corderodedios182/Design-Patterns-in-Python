3fbe8a604533216905dfce868116c9b10eee9ade
Corderodedios182 <corderodedios182@gmail.com>
2022-10-30 11:31:13 -0600

    DP : SRP (Single responsibility principle)

--- srp.py
+++ srp.py
@@ -1,4 +1,17 @@
+
+#Principio de responsabilidad única
+
+# - Una clase debe tener una sola razón para cambiar
+# - No debe cargarse con múltiples responsabilidades y una sola responsabilidad no
+#   debe distribuirse entre varias clases ni mezclarse con otras responsabilidades.
+# - La razón es que cuantos más cambios se soliciten en el futuro, más cambios 
+#   tendrá que aplicar la clase.
+
 class Journal:
+    """
+    Solo debe tener la responsabilidad de interactuar con los texto que interactuan en la clase.
+    Agregar y borrar.
+    """
     def __init__(self):
         self.entries = []
         self.count = 0
@@ -13,7 +26,9 @@ class Journal:
     def __str__(self):
         return "\n".join(self.entries)
 
-    # break SRP
+    # ¡WARNING! : estos métodos sobran en la clase caen en otra responsabilidad de la clase
+    #             que corresponde a guardar los textos.
+
     def save(self, filename):
         file = open(filename, "w")
         file.write(str(self))
@@ -26,20 +41,23 @@ class Journal:
         pass
 
 
+#Mejor práctica : El proceso de guardado debe ir en una clase con una única responsabilidad.
 class PersistenceManager:
-    def save_to_file(journal, filename):
-        file = open(filename, "w")
-        file.write(str(journal))
+    @staticmethod
+    def save_to_file(j, file):
+        file = open(file, "w")
+        file.write(str(j))
         file.close()
 
 
 j = Journal()
 j.add_entry("I cried today.")
 j.add_entry("I ate a bug.")
+j.add_entry("Hola mundo!")
 print(f"Journal entries:\n{j}\n")
 
 p = PersistenceManager()
-file = r'C:\Users\cflorelu\Documents\1_Ternium\Design-Patterns-in-Python\journal.txt'
+file = r'C:\\Users\\cflorelu\\Documents\\1_Ternium\\Design-Patterns-in-Python\\data\\journal.txt'
 p.save_to_file(j, file)
 
 # verify!
